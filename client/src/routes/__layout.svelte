65381e29c90b64b4b9085d9a0c48e34f3da78c72
Marcus Wiseman <md.wiseman@hotmail.com>
2022-03-24 15:51:26 +0100

    init commit

---
+++ client/src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import '../app.css';
+</script>
+
+<svelte:head>
+    <title>AbadiArt</title>
+</svelte:head>
+
+<div
+    class="flex flex-col items-center justify-between pt-20 w-full min-h-screen text-stone-900 select-none overflow-x-hidden"
+>
+    <div class="min-h-max w-full px-3.5 lg:w-2/3 lg:max-w-4xl lg:px-0">
+        <slot />
+    </div>
+</div>
---
+++ client/src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+    import src from '$lib/assets/images/under-construction.png';
+</script>
+
+<div class="h-full w-full flex flex-col items-center">
+    <h1 class="text-4xl font-semibold mb-16">Welcome to Abadi Art!</h1>
+    <img {src} alt="Under construction" class=" max-w-xl" />
+</div>
