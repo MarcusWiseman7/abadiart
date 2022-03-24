2bb79dbd7f7592447612a734ebba168ac621355e
Marcus Wiseman <md.wiseman@hotmail.com>
2022-03-24 18:03:03 +0100

    Resize image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class="h-full w-full flex flex-col items-center">
-    <h1 class="text-4xl font-semibold mb-16">Welcome to Abadi Art!</h1>
-    <img {src} alt="Under construction" class=" max-w-xl" />
+    <h1 class="text-3xl lg:text-4xl font-semibold mb-16">Welcome to Abadi Art!</h1>
+    <img {src} alt="Under construction" class="max-w-xs lg:max-w-xl" />
 </div>
