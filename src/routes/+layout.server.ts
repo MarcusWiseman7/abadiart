927d1da112f50dea679690b2123283aa0e75e5df
Marcus Wiseman <md.wiseman@hotmail.com>
2025-02-18 20:06:29 +0100

    test

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -11,24 +11,24 @@ export const load: LayoutServerLoad = async ({ request }) => {
     const parser = new UAParser(ua);
     const uaResult = parser.getResult();
     const userDevice = uaResult.device.type || 'desktop';
-    
-    headerAcceptedLangs?.split(',').forEach(x => {
+
+    headerAcceptedLangs?.split(',').forEach((x) => {
         const sub = x.split(';');
         const locale = sub[0].slice(0, 2);
         if (siteAcceptedLanguages.includes(locale)) {
             parsedHeaderLanguages.push({ locale, q: sub[1] ? parseFloat(sub[1].split('=')[1]) : 1 });
         }
-    });;
-    
+    });
+
     parsedHeaderLanguages.sort((a, b) => {
         if (a.q === b.q) return 0;
         if (a.q > b.q) return -1;
         return 1;
     });
-    
+
     const i18n = parsedHeaderLanguages[0]?.locale || 'en';
 
-    // fetch nav from sanity
+    // fetch nav from sanity!
     const navQuery = `*[_type == 'nav'] {navList}[0]`;
     const { navList } = await sanity.fetch(navQuery);
 
@@ -37,4 +37,4 @@ export const load: LayoutServerLoad = async ({ request }) => {
     const { image } = await sanity.fetch(logoQuery);
 
     return { data: JSON.stringify({ logo: image, navList, i18n, userDevice }) };
-}
+};
