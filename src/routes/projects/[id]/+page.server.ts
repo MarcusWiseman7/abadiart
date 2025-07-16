1799c9aa0b1ed9f28190d6ef1ebf66b40f332e82
Marcus Wiseman <md.wiseman@hotmail.com>
2025-07-16 20:58:00 +0200

    fix: update projects server

--- src/routes/projects/[id]/+page.server.ts
+++ src/routes/projects/[id]/+page.server.ts
@@ -3,21 +3,13 @@ import { parseContentToText } from '$lib/helpers';
 import type { IProject } from '$lib/ts-interfaces';
 import type { PageServerLoad } from './$types';
 
-interface IData {
-    project: IProject;
-}
-
 export const load: PageServerLoad = async ({ params }) => {
     const { id } = params;
 
-    const query = `*[_type == "projects"][0] {
-        "project": projects[] {
-            "_key": "${id}",
-            ...
-        }[0]
-    }`;
-    const res: IData = await sanity.fetch(query);
-    const description = res.project?.summary ? parseContentToText(res.project.summary) : '';
-    
-    return { data: JSON.stringify({ id, project: res.project, description }) };
-}
+    const query = `*[_type == "projects"][0].projects[_key == "${id}"][0]`;
+    const res: IProject = await sanity.fetch(query);
+
+    const description = res?.summary ? parseContentToText(res.summary) : '';
+
+    return { data: JSON.stringify({ id, project: res, description }) };
+};
